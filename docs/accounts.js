73586712d2919262293f56430d481bb5160a50de
Jason Wang <jason.b.wang@avanade.com>
2022-02-05 21:25:25 +0800

    update published page

--- docs/accounts.js
+++ docs/accounts.js
@@ -1,4 +1,4 @@
-var weather_key = "06f3b421f0334c738b65752a7b734996";
+var weather_key = "<your qweather key>";
 var weather_location = "101010100"; // or "116.41,39.92" for GPS coordinate
 var weather_lang = "zh";
 var weather_unit = "m"; // m for meter, i for mile
